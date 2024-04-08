a2387f2f55cf9b558062e43c956448122ce7d006
MarekL-MarekL <markandmark3@gmail.com>
2024-04-08 11:17:57 +0200

    dra ett kort funkar bra

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const element = document.getElementById("fetchDataBtn");
+const imageview = document.getElementById("viewcard");
 
 element.addEventListener("click", function getApi() {
-	// const inputText = document.getElementById("input").value;
 	const fullUri = `https://deckofcardsapi.com/api/deck/new/draw/?count=1`;
-	// const fullUri = `https://www.swapi.tech/api/people`;
-	// textArea.value = "Fetchig data....";
 
 	fetch(fullUri)
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data.cards[0].image);
-			// let s = data.result[0].properties;
-			// console.log(s);
-			// console.log(
-			// 	`Height: ${s.height}, Mass: ${s.mass}, Hair color: ${s.hair_color}, Eye color: ${s.eye_color}, Gender: ${s.gender}.`
-			// );
-			// textArea.value = `Height: ${s.height}\nMass: ${s.mass}\nHair color: ${s.hair_color}\nEye color: ${s.eye_color}\nGender: ${s.gender}`;
+			const fetchedImg = data.cards[0].image;
+			drawCard(fetchedImg);
 		})
 		.catch((err) => {
 			console.log(err);
-			// textArea.value = `Error: ${err.message}`;
 		});
 });
+
+function drawCard(fetchedImg) {
+	imageview.innerHTML = "";
+	const newImg = document.createElement("img");
+	newImg.setAttribute("src", fetchedImg);
+	imageview.appendChild(newImg);
+}
